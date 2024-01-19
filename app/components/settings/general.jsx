17313531e1ad6e754b9e083f3936603bce507706
ksjaay <ksjaay@gmail.com>
2024-01-19 15:00:13 +0000

    Replaces dropdowns with new components

--- app/components/settings/general.jsx
+++ app/components/settings/general.jsx
@@ -1,40 +1,21 @@
 import './tab.scss';
 import './general.scss';
 import TextInput from '../ui/input';
-import Dropdown from '../ui/dropdown';
-import timezones from '../../constant/timeformats.json';
-import timeformats from '../../constant/dateformats.json';
 import { createPostRequest } from '../../services/axios';
 import { parseUserCookie } from '../../utils/cookies';
 
+import {
+  TimezoneDropdown,
+  DateformatDropdown,
+  TimeformatDropdown,
+  ColorsDropdown,
+  ThemesDropdown,
+} from './dropdown';
+
 const SettingsGeneral = () => {
-  const userTimezone =
-    window?.localStorage?.getItem('timezone') ||
-    Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-  const userDateFormat =
-    window?.localStorage?.getItem('dateformat') ||
-    'Intl.DateTimeFormat().resolvedOptions().timeZone';
-
-  const userTimeFormat =
-    window?.localStorage?.getItem('timeformat') ||
-    'Intl.DateTimeFormat().resolvedOptions().timeZone';
-
   const user = parseUserCookie(window?.document?.cookie);
 
   const handleUpdate = async (e, type) => {
-    if (type === 'timezone' || type === 'dateformat' || type === 'timeformat') {
-      window.localStorage.setItem(type, e.target.value);
-    }
-
-    // if (type === 'color') {
-    //   console.log(e.target.value);
-    // }
-
-    // if (type === 'theme') {
-    //   console.log(e.target.value);
-    // }
-
     if (type === 'displayName') {
       await createPostRequest('/user/update', {
         displayName: e.target.value,
@@ -62,30 +43,7 @@ const SettingsGeneral = () => {
           <div className="settings-general-subtitle">
             Configure timezone and date format display settings
           </div>
-          <Dropdown
-            label="Avatar"
-            options={[]}
-            onChange={(e) => handleUpdate(e, 'avatar')}
-          />
-          {/* <Dropdown
-            label="Color"
-            options={[
-              { name: 'Blue', value: 'blue' },
-              { name: 'Purple', value: 'purple' },
-              { name: 'Green', value: 'green' },
-              { name: 'Yellow', value: 'yellow' },
-              { name: 'Red', value: 'red' },
-              { name: 'Cyan', value: 'cyan' },
-              { name: 'Pink', value: 'pink' },
-            ]}
-          />
-          <Dropdown
-            label="Theme"
-            options={[
-              { name: 'Dark', value: 'dark' },
-              { name: 'Light', value: 'light' },
-            ]}
-          /> */}
+
           <TextInput
             label="Username"
             defaultValue={user.displayName}
@@ -93,35 +51,19 @@ const SettingsGeneral = () => {
               handleUpdate(e, 'displayName');
             }}
           />
+
+          <ThemesDropdown />
+          <ColorsDropdown />
         </div>
         <div className="settings-general-date">
           <div className="settings-general-title">Date & Time</div>
           <div className="settings-general-subtitle">
             Configure profile information and appearance
           </div>
-          <Dropdown
-            label="Timezone"
-            options={timezones}
-            defaultValue={userTimezone}
-            onChange={(e) => handleUpdate(e, 'timezone')}
-          />
-          <Dropdown
-            label="Date Format"
-            defaultValue={userDateFormat}
-            options={timeformats}
-            onChange={(e) => handleUpdate(e, 'dateformat')}
-          />
-          <Dropdown
-            label="Time Format"
-            value={userTimeFormat}
-            options={[
-              { name: '23:59:59', value: '4' },
-              { name: '23:59', value: '3' },
-              { name: '11:59', value: '2' },
-              { name: '11:59 PM', value: '1' },
-            ]}
-            onChange={(e) => handleUpdate(e, 'timeformat')}
-          />
+
+          <TimezoneDropdown />
+          <DateformatDropdown />
+          <TimeformatDropdown />
         </div>
       </div>
     </div>
