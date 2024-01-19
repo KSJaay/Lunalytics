f9026501d26142bc8c0ccb961d3803215380a4ab
ksjaay <ksjaay@gmail.com>
2024-01-19 23:57:53 +0000

    Updates createURL to process current url query parameters

--- server/utils/url.js
+++ server/utils/url.js
@@ -1,6 +1,24 @@
 const createURL = (path = '', params) => {
   const appUrl = process.env.MODE === 'production' ? '' : process.env.APP_URL;
-  if (!params) return `${appUrl}${path}`;
+
+  if (!path || !params) return `${appUrl}${path}`;
+
+  if (path.includes('?')) {
+    const [pathWithoutQuery, queries] = path.split('?');
+    const queriesAsObject = {};
+
+    queries.split('&').forEach((query) => {
+      const [key, value] = query.split('=');
+      queriesAsObject[key] = value;
+    });
+
+    const searchParams = new URLSearchParams({
+      ...queriesAsObject,
+      ...params,
+    }).toString();
+
+    return `${appUrl}${pathWithoutQuery}?${searchParams}`;
+  }
 
   const searchParams = new URLSearchParams(params).toString();
 
