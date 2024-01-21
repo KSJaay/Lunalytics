8e3bc8e2ca8cae0cc13c1b97f37b75dd26efb61d
ksjaay <ksjaay@gmail.com>
2024-01-21 23:22:36 +0000

    Moves from nanoid to uuid

--- server/utils/randomId.js
+++ server/utils/randomId.js
@@ -1,11 +1,7 @@
-const { customAlphabet } = require('nanoid');
+const { v4: uuidv4 } = require('uuid');
 
 const randomId = () => {
-  // string containing all alphanumeric characters
-  const alphaNumbericString = '0123456789abcdefghijklmnopqrstuvwxyz';
-  const nanoid = customAlphabet(alphaNumbericString, 16);
-
-  return nanoid();
+  return uuidv4();
 };
 
 module.exports = randomId;
