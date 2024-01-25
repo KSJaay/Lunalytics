99423ec007ecef5fc942b4d60609adf9830e1b42
ksjaay <ksjaay@gmail.com>
2024-01-25 16:49:01 +0000

    Adds glassmorph option for modals

--- app/context/modal.js
+++ app/context/modal.js
@@ -4,6 +4,7 @@ export default class ModalStore {
   constructor() {
     this.isOpen = false;
     this.content = null;
+    this.glassmorph = true;
     makeObservable(this, {
       isOpen: observable,
       openModal: action,
@@ -11,13 +12,15 @@ export default class ModalStore {
     });
   }
 
-  openModal = (content) => {
+  openModal = (content, glassmorph = true) => {
     this.isOpen = true;
     this.content = content;
+    this.glassmorph = glassmorph;
   };
 
   closeModal = () => {
     this.isOpen = false;
     this.content = null;
+    this.glassmorph = true;
   };
 }
