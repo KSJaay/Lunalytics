9eb17df421d58a4956bb0eeb82a601854ce527cd
ksjaay <ksjaay@gmail.com>
2024-01-27 18:16:15 +0000

    Updates validators and adds /exists route

--- server/utils/errors.js
+++ server/utils/errors.js
@@ -16,28 +16,45 @@ class UnprocessableError extends Error {
   }
 }
 
+class ConflictError extends Error {
+  constructor(error) {
+    super();
+    this.name = 'ConflictError';
+    this.error = error;
+  }
+}
+
 const handleError = (error, response) => {
-  logger.error('Handler', error);
+  logger.error('Error handler', error.message + error.stack);
 
-  if (error instanceof AuthorizationError) {
-    return response.status(401).send({
-      message: error.error,
+  if (!response.headersSent) {
+    if (error instanceof AuthorizationError) {
+      return response.status(401).send({
+        message: error.error,
+      });
+    }
+
+    if (error instanceof ConflictError) {
+      return response.status(409).send({
+        message: error.error,
+      });
+    }
+
+    if (error instanceof UnprocessableError) {
+      return response.status(422).send({
+        message: error.error,
+      });
+    }
+
+    return response.status(500).send({
+      message: 'Something went wrong',
     });
   }
-
-  if (error instanceof UnprocessableError) {
-    return response.status(422).send({
-      message: error.error,
-    });
-  }
-
-  return response.status(500).send({
-    message: 'Something went wrong',
-  });
 };
 
 module.exports = {
   AuthorizationError,
+  ConflictError,
   UnprocessableError,
   handleError,
 };
