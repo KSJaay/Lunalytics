660242bd9baec2033a0583659fd1a6fdf169359f
ksjaay <ksjaay@gmail.com>
2024-02-10 02:10:25 +0000

    Adds /api/monitor/status endpoint

---
+++ server/middleware/monitor/status.js
@@ -0,0 +1,68 @@
+const cache = require('../../cache');
+const validTypes = ['latest', 'day', 'week', 'month'];
+
+const fetchMonitorStatus = (request, response) => {
+  const { monitorId, type } = request.query;
+
+  if (!monitorId) {
+    return response.status(400).json({
+      message: 'Monitor ID is required',
+    });
+  }
+
+  if (!validTypes.includes(type)) {
+    return response.status(400).json({
+      message: 'Invalid type',
+    });
+  }
+
+  if (!cache.monitors.get(monitorId)) {
+    return response.status(404).json({
+      message: 'Monitor not found',
+    });
+  }
+
+  if (type === 'latest') {
+    const heartbeats = cache.heartbeats.getRealtime(monitorId);
+
+    if (heartbeats.length < 2) {
+      return response.sendStatus(416);
+    }
+
+    return response.json(heartbeats);
+  }
+
+  if (type === 'day') {
+    const heartbeats = cache.heartbeats.getDaily(monitorId);
+
+    if (heartbeats.length < 2) {
+      return response.sendStatus(416);
+    }
+
+    return response.json(heartbeats);
+  }
+
+  if (type === 'week') {
+    const heartbeats = cache.heartbeats.getWeekly(monitorId);
+
+    if (heartbeats.length < 2) {
+      return response.sendStatus(416);
+    }
+
+    return response.json(heartbeats);
+  }
+
+  if (type === 'month') {
+    const heartbeats = cache.heartbeats.getMonthly(monitorId);
+
+    if (heartbeats.length < 2) {
+      return response.sendStatus(416);
+    }
+
+    return response.json(heartbeats);
+  }
+
+  return response.sendStatus(404);
+};
+
+module.exports = fetchMonitorStatus;
--- server/routes/monitor.js
+++ server/routes/monitor.js
@@ -8,10 +8,12 @@ const monitorEdit = require('../middleware/monitor/edit');
 const monitorDelete = require('../middleware/monitor/delete');
 const fetchMonitorUsingId = require('../middleware/monitor/id');
 const hasEditorPermissions = require('../middleware/user/hasEditor');
+const fetchMonitorStatus = require('../middleware/monitor/status');
 
 router.post('/add', hasEditorPermissions, monitorAdd);
 router.post('/edit', hasEditorPermissions, monitorEdit);
 router.get('/delete', hasEditorPermissions, monitorDelete);
+router.get('/status', fetchMonitorStatus);
 router.get('/id', fetchMonitorUsingId);
 
 module.exports = router;
