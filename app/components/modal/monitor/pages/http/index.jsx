c283199785baada896701a1bda749e75605c4d49
ksjaay <ksjaay@gmail.com>
2024-03-23 02:01:59 +0000

    Creates monitor add http modal components

---
+++ app/components/modal/monitor/pages/http/index.jsx
@@ -0,0 +1,58 @@
+// import dependencies
+import PropTypes from 'prop-types';
+
+// import local files
+import TextInput from '../../../../ui/input';
+import MonitorHttpMethods from './methods';
+import MonitorHttpStatusCodes from './statusCodes';
+
+const MonitorAddHttp = ({ inputs, errors, handleInput }) => {
+  const handleMethodSelect = (method) => {
+    handleInput('method', method);
+  };
+
+  return (
+    <div style={{ minHeight: '300px', width: '400px' }}>
+      <TextInput
+        label="URL"
+        value={inputs.url}
+        onChange={(event) => {
+          handleInput('url', event.target.value);
+        }}
+        error={errors.url}
+      />
+      <MonitorHttpMethods
+        selectValue={inputs.method}
+        handleSelect={handleMethodSelect}
+      />
+      {errors.method && (
+        <label className="text-input-error">{errors.method}</label>
+      )}
+
+      <MonitorHttpStatusCodes
+        selectedIds={inputs.valid_status_codes}
+        handleStatusCodeSelect={(code) => {
+          const { valid_status_codes = [] } = inputs;
+          const validStatusCodes = valid_status_codes.includes(code)
+            ? valid_status_codes.filter((id) => id !== code)
+            : valid_status_codes.concat(code);
+          handleInput('valid_status_codes', validStatusCodes);
+        }}
+      />
+
+      {errors.valid_status_codes && (
+        <label className="text-input-error">{errors.valid_status_codes}</label>
+      )}
+    </div>
+  );
+};
+
+MonitorAddHttp.displayName = 'MonitorAddHttp';
+
+MonitorAddHttp.propTypes = {
+  inputs: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  handleInput: PropTypes.func.isRequired,
+};
+
+export default MonitorAddHttp;
---
+++ app/components/modal/monitor/pages/http/statusCodes.jsx
@@ -0,0 +1,60 @@
+// import dependencies
+import PropTypes from 'prop-types';
+
+// import local files
+import useSelect from '../../../../../hooks/useSelect';
+import Select from '../../../../ui/select';
+import statusCodes from '../../../../../constant/statusCodes.json';
+
+const MonitorHttpStatusCodes = ({
+  selectedIds = [],
+  handleStatusCodeSelect,
+}) => {
+  const { selectIsOpen, selectSearch, handleSearch, toggleSelect } =
+    useSelect(false);
+
+  const filteredStatusCodes = statusCodes.filter((code) =>
+    code.includes(selectSearch)
+  );
+
+  return (
+    <>
+      <label className="text-input-label">Accepted Status Codes</label>
+      <Select.Container isOpen={selectIsOpen} toggleSelect={toggleSelect}>
+        <Select.Trigger
+          asInput
+          isOpen={selectIsOpen}
+          toggleSelect={toggleSelect}
+        >
+          {selectedIds.join(', ')}
+        </Select.Trigger>
+        <Select.List
+          fullWidth
+          isOpen={selectIsOpen}
+          selectSearch={selectSearch}
+          handleSearch={handleSearch}
+        >
+          {filteredStatusCodes.map((code) => (
+            <Select.Item
+              key={code}
+              onClick={() => handleStatusCodeSelect(code)}
+              showDot
+              isSelected={selectedIds.includes(code)}
+            >
+              {code}
+            </Select.Item>
+          ))}
+        </Select.List>
+      </Select.Container>
+    </>
+  );
+};
+
+MonitorHttpStatusCodes.displayName = 'MonitorHttpStatusCodes';
+
+MonitorHttpStatusCodes.propTypes = {
+  selectedIds: PropTypes.array,
+  handleStatusCodeSelect: PropTypes.func.isRequired,
+};
+
+export default MonitorHttpStatusCodes;
