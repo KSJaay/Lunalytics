54770149b6ac4225eaf18c8806c15b652bc61bab
ksjaay <ksjaay@gmail.com>
2024-03-24 02:39:12 +0000

    Adds tcpPing to check status of ip:port

---
+++ server/tools/tcpPing.js
@@ -0,0 +1,67 @@
+// import dependencies
+const net = require('net');
+
+// import local files
+const logger = require('../utils/logger');
+
+const tcpStatusCheck = async (monitor, callback) => {
+  const socket = new net.Socket();
+  const startTime = Date.now();
+
+  const options = {
+    address: monitor.url || '127.0.0.1',
+    port: monitor.port || 80,
+    timeout: monitor.interval * 1000 || 5000,
+  };
+
+  socket.setTimeout(options.timeout, function () {
+    const latency = Date.now() - startTime;
+    socket.destroy();
+
+    logger.error(
+      'TCP Status Check',
+      `Issue checking monitor ${monitor.monitorId}: TIMED OUT`
+    );
+
+    callback(monitor, {
+      monitorId: monitor.monitorId,
+      status: 'TIMEOUT',
+      latency,
+      message: 'Ping timed out!',
+      isDown: 1,
+    });
+  });
+
+  socket.connect(options.port, options.address, function () {
+    const latency = Date.now() - startTime;
+    socket.destroy();
+
+    callback(monitor, {
+      monitorId: monitor.monitorId,
+      status: 'ALIVE',
+      latency,
+      message: 'Up and running!',
+      isDown: 0,
+    });
+  });
+
+  socket.on('error', function (error) {
+    const latency = Date.now() - startTime;
+    socket.destroy();
+
+    logger.error(
+      'TCP Status Check',
+      `Issue checking monitor ${monitor.monitorId}: ${error.message} `
+    );
+
+    callback(monitor, {
+      monitorId: monitor.monitorId,
+      status: 'ERROR',
+      latency,
+      message: error.message,
+      isDown: 1,
+    });
+  });
+};
+
+module.exports = tcpStatusCheck;
