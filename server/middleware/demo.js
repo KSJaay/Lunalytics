0c922a40f7a1341420c3208372f4bfb65664b399
ksjaay <ksjaay@gmail.com>
2024-05-04 20:19:05 +0100

    Fixes issues with demo not working properly

--- server/middleware/demo.js
+++ server/middleware/demo.js
@@ -2,7 +2,9 @@ import { getDemoUser } from '../database/queries/user.js';
 import { setServerSideCookie } from '../utils/cookies.js';
 
 const isDemo = async (request, response, next) => {
-  if (process.env.IS_DEMO) {
+  const { demo } = request.query;
+
+  if (process.env.IS_DEMO === true && demo === 'true') {
     const cookie = await getDemoUser();
 
     setServerSideCookie(response, 'access_token', cookie);
