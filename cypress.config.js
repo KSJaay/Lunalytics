84ffa7f2c1ccd3a960e42e17f83e898e5be21243
ksjaay <ksjaay@gmail.com>
2024-05-13 00:37:25 +0100

    Adds config for cypress end to end testing

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:2308',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    specPattern: 'test/e2e/**/*.test.{js,jsx,ts,tsx}',
+    fixturesFolder: 'test/e2e/setup/fixtures',
+    screenshotsFolder: 'test/e2e/setup/screenshots',
+    videosFolder: 'test/e2e/setup/videos',
+    downloadsFolder: 'test/e2e/setup/downloads',
+    supportFile: 'test/e2e/setup/support/e2e.js',
+  },
+});
