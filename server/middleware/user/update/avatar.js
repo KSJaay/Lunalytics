22a7c170df15c0f91cf352d07c0c8815e4f9e2c0
ksjaay <ksjaay@gmail.com>
2024-05-14 16:38:58 +0100

    Fixes bugs found while writing tests

--- server/middleware/user/update/avatar.js
+++ server/middleware/user/update/avatar.js
@@ -2,28 +2,41 @@ import {
   userExists,
   updateUserAvatar,
 } from '../../../database/queries/user.js';
+import { handleError } from '../../../utils/errors.js';
 import validators from '../../../utils/validators/index.js';
 
 const userUpdateAvatar = async (request, response) => {
-  const { access_token } = request.cookies;
+  try {
+    const { access_token } = request.cookies;
 
-  const user = await userExists(access_token);
+    if (!access_token) {
+      return response.sendStatus(401);
+    }
 
-  const { avatar } = request.body;
+    const user = await userExists(access_token);
 
-  if (!avatar || user.avatar === avatar) {
+    if (!user) {
+      return response.sendStatus(401);
+    }
+
+    const { avatar } = request.body;
+
+    if (!avatar || user.avatar === avatar) {
+      return response.sendStatus(200);
+    }
+
+    const isInvalidAvatar = validators.user.isAvatar(avatar);
+
+    if (isInvalidAvatar) {
+      return response.status(400).send(isInvalidAvatar);
+    }
+
+    await updateUserAvatar(user.email, avatar);
+
     return response.sendStatus(200);
+  } catch (error) {
+    handleError(error, response);
   }
-
-  const isInvalidAvatar = validators.user.isAvatar(avatar);
-
-  if (isInvalidAvatar) {
-    return response.status(400).send(isInvalidAvatar);
-  }
-
-  await updateUserAvatar(user.email, avatar);
-
-  return response.sendStatus(200);
 };
 
 export default userUpdateAvatar;
