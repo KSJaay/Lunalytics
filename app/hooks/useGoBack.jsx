e3d093c50c30f459ebcc21558c5191d49caabc58
ksjaay <ksjaay@gmail.com>
2024-06-01 23:20:16 +0100

    Adds useGoBack hook for settings page

---
+++ app/hooks/useGoBack.jsx
@@ -0,0 +1,19 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+/**
+ * A function that returns a function to navigate back to the previous location or a fallback location.
+ *
+ * @param {Object} prev - an object containing the previous location and fallback location
+ * @return {void}
+ */
+const useGoBack = () => {
+  const { key: prevKey } = useLocation();
+  const navigate = useNavigate();
+
+  return ({ fallback = '/' } = {}) => {
+    const key = prevKey !== 'default' ? -1 : fallback;
+    navigate(key);
+  };
+};
+
+export default useGoBack;
