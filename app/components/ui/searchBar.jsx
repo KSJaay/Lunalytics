e2e4e2e329097922b5fc9e0bbca37514a373859f
ksjaay <ksjaay@gmail.com>
2024-06-01 23:19:01 +0100

    Fixes some issues with ui components

--- app/components/ui/searchBar.jsx
+++ app/components/ui/searchBar.jsx
@@ -4,11 +4,17 @@ import './searchBar.scss';
 // import dependencies
 import PropTypes from 'prop-types';
 
-const SearchBar = ({ label, id, ...props }) => {
+const SearchBar = ({ label, id, tabIndex = 0, ...props }) => {
   return (
     <>
       {label && <label className="search-bar-input-label">{label}</label>}
-      <input type="text" className="search-bar-input" id={id} {...props} />
+      <input
+        type="text"
+        className="search-bar-input"
+        id={id}
+        tabIndex={tabIndex}
+        {...props}
+      />
     </>
   );
 };
@@ -18,6 +24,7 @@ SearchBar.displayName = 'SearchBar';
 SearchBar.propTypes = {
   label: PropTypes.string,
   id: PropTypes.string,
+  tabIndex: PropTypes.number,
 };
 
 export default SearchBar;
