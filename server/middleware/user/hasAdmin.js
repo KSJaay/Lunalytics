6e6bf9cb829c483cef5fa45720ef32e4b59d11ca
ksjaay <ksjaay@gmail.com>
2024-06-03 01:32:18 +0100

    Updates/creates new middleware for settings pages

--- server/middleware/user/hasAdmin.js
+++ server/middleware/user/hasAdmin.js
@@ -1,23 +1,28 @@
 import { userExists } from '../../database/queries/user.js';
+import { handleError } from '../../utils/errors.js';
 
 const hasAdminPermissions = async (request, response, next) => {
-  const { access_token } = request.cookies;
+  try {
+    const { access_token } = request.cookies;
 
-  if (!access_token) {
-    return response.sendStatus(401);
+    if (!access_token) {
+      return response.sendStatus(401);
+    }
+
+    const user = await userExists(access_token);
+
+    if (!user) {
+      return response.sendStatus(401);
+    }
+
+    if (user.permission !== 1 && user.permission !== 2) {
+      return response.sendStatus(401);
+    }
+
+    return next();
+  } catch (error) {
+    handleError(error, response);
   }
-
-  const user = await userExists(access_token);
-
-  if (!user) {
-    return response.sendStatus(401);
-  }
-
-  if (user.permission !== 1 && user.permission !== 2) {
-    return response.sendStatus(401);
-  }
-
-  return next();
 };
 
 export default hasAdminPermissions;
