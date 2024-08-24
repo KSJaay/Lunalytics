c4477d4d1a909253d07c5ad866f8cff40b750635
ksjaay <ksjaay@gmail.com>
2024-08-24 15:31:47 +0100

    Moves migration scripts to scripts folder

--- scripts/migrations/tcpUpdate-0-4-0.js
+++ scripts/migrations/tcpUpdate-0-4-0.js
@@ -1,5 +1,5 @@
 // import local files
-import SQLite from '../database/sqlite/setup.js';
+import SQLite from '../../server/database/sqlite/setup.js';
 import logger from '../../shared/utils/logger.js';
 
 const infomation = {
@@ -7,6 +7,7 @@ const infomation = {
   description:
     'Adds support for TCP pings. This allows you to monitor TCP services such as databases, web servers, and more. This update also adds a new column to the monitor table to store the type of monitor, and port for tcp monitors.',
   version: '0.4.0',
+  breaking: true,
 };
 
 const migrate = async () => {
