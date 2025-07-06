494451dfe88aaece3ca3742bb41e79bf8dbe7a3c
ksjaay <ksjaay@gmail.com>
2025-07-06 15:24:56 +0100

    Adds error logging for setup page

--- app/handlers/setup.js
+++ app/handlers/setup.js
@@ -7,20 +7,30 @@ import { getSetupKeys } from '../../shared/data/setup';
 import setupValidators from '../../shared/validators/setup';
 
 const submitSetup = async (setErrors, type = 'basic', inputs) => {
-  const keys = getSetupKeys(type);
+  try {
+    const keys = getSetupKeys(type);
 
-  let errors = false;
-  for (let key of keys) {
-    const validator = setupValidators[key];
-    errors = validator(inputs[key], setErrors);
+    let errors = false;
+    for (let key of keys) {
+      const validator = setupValidators[key];
+      errors = validator(inputs[key], setErrors);
 
-    if (errors) break;
+      if (errors) break;
+    }
+
+    if (errors) throw new Error('Issue occured while setting up configuration');
+
+    await createPostRequest('/auth/setup', { ...inputs, type });
+    toast.success('Setup configuration successful');
+  } catch (error) {
+    if (error?.response?.data) {
+      toast.error(error?.response?.data?.general);
+    } else {
+      toast.error('Error while setting up configuration');
+    }
+
+    throw error;
   }
-
-  if (errors) throw new Error('Issue occured while setting up configuration');
-
-  await createPostRequest('/auth/setup', { ...inputs, type });
-  toast.success('Setup configuration successful');
 };
 
 export default submitSetup;
