1287c89ba4f5be1dac6731a1f331b4bb78d6d76c
ksjaay <ksjaay@gmail.com>
2025-07-07 17:10:41 +0100

    Moves to new useFetch hook to introduce loading screen

--- app/layout/global.jsx
+++ app/layout/global.jsx
@@ -1,18 +1,18 @@
 // import dependencies
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useNavigate } from 'react-router-dom';
 
 // import local files
 import Modal from '../components/ui/modal';
 import useContextStore from '../context';
-import { createGetRequest } from '../services/axios';
 import { fetchMonitorById } from '../services/monitor/fetch';
 import {
   LocalStorageStateProvider,
   useLocalStorageState,
 } from '../hooks/useLocalstorage';
+import Loading from '../components/ui/loading';
+import useFetch from '../hooks/useFetch';
 
 const GlobalLayout = ({ children }) => {
   const {
@@ -28,35 +28,58 @@ const GlobalLayout = ({ children }) => {
 
   const localStorageState = useLocalStorageState();
 
-  useEffect(() => {
-    const fetchMontiors = async () => {
-      try {
-        const user = await createGetRequest('/api/user');
-        const monitors = await createGetRequest('/api/user/monitors');
-        const notifications = await createGetRequest('/api/notifications');
-        const statusPages = await createGetRequest('/api/status-pages');
-        const incidents = await createGetRequest('/api/incident/all');
-        const data = monitors?.data || [];
-
-        setUser(user?.data);
-        setMonitors(data);
-        setTimeouts(data, fetchMonitorById);
-        setNotifications(notifications?.data || []);
-        setStatusPages(statusPages?.data || []);
-        setIncidents(incidents?.data || []);
-      } catch (error) {
-        if (error.response?.status === 401) {
-          return navigate('/login');
-        }
-        if (error.response?.status === 403) {
-          return navigate('/verify');
-        }
-        navigate('/error');
+  const { isLoading: isUserLoading } = useFetch({
+    url: '/api/user',
+    onSuccess: (data) => setUser(data),
+    onFailure: (error) => {
+      if (error.response?.status === 401) {
+        return navigate('/login');
       }
-    };
 
-    fetchMontiors();
-  }, []);
+      if (error.response?.status === 403) {
+        return navigate('/verify');
+      }
+
+      navigate('/error');
+    },
+  });
+
+  const { isLoading: isMonitorsLoading } = useFetch({
+    url: '/api/user/monitors',
+    onSuccess: (data) => {
+      setMonitors(data);
+      setTimeouts(data, fetchMonitorById);
+    },
+    onFailure: () => navigate('/error'),
+  });
+
+  const { isLoading: isNotificationsLoading } = useFetch({
+    url: '/api/notifications',
+    onSuccess: (data) => setNotifications(data),
+    onFailure: () => navigate('/error'),
+  });
+
+  const { isLoading: isStatusPagesLoading } = useFetch({
+    url: '/api/status-pages',
+    onSuccess: (data) => setStatusPages(data),
+    onFailure: () => navigate('/error'),
+  });
+
+  const { isLoading: isIncidentsLoading } = useFetch({
+    url: '/api/incident/all',
+    onSuccess: (data) => setIncidents(data),
+    onFailure: () => navigate('/error'),
+  });
+
+  if (
+    isUserLoading ||
+    isMonitorsLoading ||
+    isNotificationsLoading ||
+    isStatusPagesLoading ||
+    isIncidentsLoading
+  ) {
+    return <Loading />;
+  }
 
   return (
     <LocalStorageStateProvider value={localStorageState}>
