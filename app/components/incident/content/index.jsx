dfe3f50c97cf140fde12bc7fbf2b23803b89f00d
ksjaay <ksjaay@gmail.com>
2025-07-13 00:54:54 +0100

    Moves incidents page to the new UI

---
+++ app/components/incident/content/index.jsx
@@ -0,0 +1,57 @@
+// import dependencies
+import { Button, Input } from '@lunalytics/ui';
+
+// import local files
+import IncidentContentMessages from './message';
+import IncidentContentHeader from './header';
+import IncidentContentImpact from './impact';
+import { observer } from 'mobx-react-lite';
+import useContextStore from '../../../context';
+import IncidentAddUpdateModal from '../../modal/incident/addUpdate';
+
+const IncidentContent = ({ incident }) => {
+  const {
+    modalStore: { openModal },
+  } = useContextStore();
+
+  return (
+    <div
+      style={{
+        padding: '1rem 1rem 7rem 1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+      }}
+    >
+      <IncidentContentHeader incident={incident} />
+
+      <div style={{ marginTop: '-0.5rem' }}>
+        <Input
+          title="Title"
+          subtitle="Breif description (maximum of 100 characters)"
+          value={incident.title}
+        />
+        <IncidentContentImpact incident={incident} />
+      </div>
+
+      <IncidentContentMessages incident={incident} />
+
+      <Button
+        fullWidth
+        color="gray"
+        variant="flat"
+        onClick={() =>
+          openModal(<IncidentAddUpdateModal incidentId={incident.incidentId} />)
+        }
+      >
+        Add an update
+      </Button>
+    </div>
+  );
+};
+
+IncidentContent.displayName = 'IncidentContent';
+
+IncidentContent.propTypes = {};
+
+export default observer(IncidentContent);
---
+++ app/components/incident/list.jsx
@@ -0,0 +1,51 @@
+// import dependencies
+import classNames from 'classnames';
+import { FaClock } from 'react-icons/fa';
+import { FaCircleCheck } from 'react-icons/fa6';
+import { IoWarning } from 'react-icons/io5';
+import { RiIndeterminateCircleFill } from 'react-icons/ri';
+
+const incidentTypes = {
+  Investigating: <RiIndeterminateCircleFill size={24} color="var(--red-700)" />,
+  Identified: <IoWarning size={24} color="var(--yellow-700)" />,
+  Monitoring: <FaClock size={24} color="var(--blue-700)" />,
+  Resolved: <FaCircleCheck size={24} color="var(--green-700)" />,
+};
+
+const NotificationsList = ({
+  incidents = [],
+  selectedIncidentId,
+  setActiveIncident,
+}) => {
+  return (
+    <div className="navigation-incident-items">
+      {incidents.map((incident) => {
+        const classes = classNames('item', {
+          'item-active': selectedIncidentId === incident.incidentId,
+        });
+
+        return (
+          <div
+            className={classes}
+            key={incident.incidentId}
+            onClick={() => setActiveIncident(incident)}
+          >
+            <div className="content">
+              <div>{incident.title}</div>
+              <span>{incident.status}</span>
+            </div>
+            <div className="icon-container">
+              {incidentTypes[incident.status]}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+NotificationsList.displayName = 'NotificationsList';
+
+NotificationsList.propTypes = {};
+
+export default NotificationsList;
