b9c663e68d5be37acc61215a39ec2b74e834081c
ksjaay <ksjaay@gmail.com>
2025-10-28 22:02:24 +0000

    Adds tests for notification middleware

---
+++ test/server/middleware/notification/test.test.js
@@ -0,0 +1,85 @@
+import { createRequest, createResponse } from 'node-mocks-http';
+import { handleError } from '../../../../server/utils/errors.js';
+import { UnprocessableError } from '../../../../shared/utils/errors.js';
+import NotificationServices from '../../../../server/notifications/index.js';
+import NotificationValidators from '../../../../shared/validators/notifications/index.js';
+import NotificationTestMiddleware from '../../../../server/middleware/notifications/test.js';
+
+vi.mock('../../../../server/utils/errors.js');
+vi.mock('../../../../shared/utils/errors.js');
+vi.mock('../../../../shared/validators/notifications/index.js');
+vi.mock('../../../../server/notifications/index.js');
+
+describe('NotificationTestMiddleware', () => {
+  let fakeRequest, fakeResponse;
+  beforeEach(() => {
+    fakeRequest = createRequest();
+    fakeResponse = createResponse();
+
+    NotificationServices.Discord = vi
+      .fn()
+      .mockImplementation(() => ({ test: vi.fn(() => Promise.resolve()) }));
+
+    NotificationValidators.Discord = vi.fn((data) => ({
+      ...data,
+      platform: 'Discord',
+      valid: true,
+    }));
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should call handleError if platform is invalid', async () => {
+    fakeRequest.body = { platform: 'invalid' };
+
+    await NotificationTestMiddleware(fakeRequest, fakeResponse);
+
+    expect(handleError).toHaveBeenCalledWith(
+      expect.any(UnprocessableError),
+      fakeResponse
+    );
+  });
+
+  it('should call handleError if service class is missing', async () => {
+    NotificationValidators.Discord.mockReturnValueOnce({
+      platform: 'notfound',
+    });
+
+    fakeRequest.body = { platform: 'Discord', data: {} };
+
+    await NotificationTestMiddleware(fakeRequest, fakeResponse);
+
+    expect(handleError).toHaveBeenCalledWith(
+      expect.any(UnprocessableError),
+      fakeResponse
+    );
+  });
+
+  it('should test notification and return 200', async () => {
+    fakeRequest.body = { platform: 'Discord', data: {} };
+
+    await NotificationTestMiddleware(fakeRequest, fakeResponse);
+
+    expect(NotificationValidators.Discord).toHaveBeenCalled();
+    expect(NotificationServices.Discord).toHaveBeenCalled();
+    expect(fakeResponse._getStatusCode()).toBe(200);
+    expect(fakeResponse._getData()).toBe('Test notification sent');
+    expect(handleError).not.toHaveBeenCalled();
+  });
+
+  it('should call handleError if service.test throws', async () => {
+    NotificationServices.Discord.mockImplementationOnce(() => ({
+      test: vi.fn(() => {
+        throw new Error('fail');
+      }),
+    }));
+
+    fakeRequest.body = { platform: 'Discord', data: {} };
+
+    await NotificationTestMiddleware(fakeRequest, fakeResponse);
+
+    expect(handleError).toHaveBeenCalledWith(expect.any(Error), fakeResponse);
+  });
+});
