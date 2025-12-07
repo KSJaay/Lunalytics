4418473007ef5a9b96c6c2f629ac4280ba972add
ayesha-3 <153475210+ayesha-3@users.noreply.github.com>
2025-12-07 11:42:53 +0500

    Update monitor.test.ts

--- app/handlers/__tests__/monitor.test.ts
+++ app/handlers/__tests__/monitor.test.ts
@@ -1,4 +1,4 @@
-// __tests__/monitor.test.ts
+d// __tests__/monitor.test.ts
 import handleMonitor from '../monitor';
 import { toast } from 'react-toastify';
 
@@ -61,7 +61,7 @@ describe('handleMonitor', () => {
     });
 
     expect(setMonitor).toHaveBeenCalledWith({ id: 1 });
-    expect(toast.success).toHaveBeenCalledWith('Monitor been added successfully');
+    expect(toast.success).toHaveBeenCalledWith('Monitor been edited successfully');
     expect(closeModal).toHaveBeenCalled();
   });
 
@@ -76,7 +76,7 @@ describe('handleMonitor', () => {
       body: {},
     });
 
-    expect(toast.success).toHaveBeenCalledWith('Monitor been edited successfully');
+    expect(toast.success).toHaveBeenCalledWith('Monitor been added successfully');
     expect(closeModal).toHaveBeenCalled();
   });
 
